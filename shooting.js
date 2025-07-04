4abfc4faa9bae7188f7ddc927d3e9e24c603a4af
Chillagma <avatar129@hotmail.com>
2025-07-04 12:11:48 -0700

    done

--- shooting.js
+++ shooting.js
@@ -1,32 +1,102 @@
 function handleProjectilesAndDraw() {
+
+  const initialSize = 500;
+const duration = 5000; 
   const minimapScale = 0.2;
+  const elapsed = performance.now();
+  const shrinkFactor = Math.min(elapsed / duration, 1);
+  var currentSize = initialSize * (1 - shrinkFactor);
 
   for (let i = projectiles.length - 1; i >= 0; i--) {
-   the=p;
+   
     var p = projectiles[i];
 
     // Move bullet
-    p.x += p.dx;
-    p.y += p.dy;
-
+    //multiply by 15 later...
+    p.x += p.dx*15;
+    p.y += p.dy*15;
+  var canvas_center_x=945;
+    var canvas_center_y=420;
     // Check map collision
     let mapX = Math.floor(p.x / tileSize);
     let mapY = Math.floor(p.y / tileSize);
     if (map[mapY]?.[mapX] === 1) {
       projectiles.splice(i, 1); // Remove bullet on wall hit
-      ctx.fillRect(p.x - 2, p.y - 2, 4, 4); // Draw hit point
+   
+       // Draw hit point
       continue;
     }
 
-    // Draw on main view
+
+    
+   
+
+//think in 2d..
+count++;
+if (count < 50) {
+      if (map[mapY]?.[mapX] === 0) {
+
+          //var mapX1 = Math.floor((player.x + cos * dist) / tileSize);
+            //var mapY1 = Math.floor((player.y + sin * dist) / tileSize);
+
+
+
+      /* ctx.fillRect(
+         Math.abs(canvas_center_x - count - p.x),
+         Math.abs(canvas_center_y - count - p.y),
+         Math.abs(500 - count - p.x),
+         Math.abs(500 - count - p.y)
+       );*/
+
+
+       
   
+       // Draw hit point
+      
+    
+
+}
+    requestAnimationFrame(handleProjectilesAndDraw);
+  }
+
+
+
+    // Draw on main view
+   
     // Draw on minimap
     ctx.fillStyle = "orange";
     ctx.fillRect(
-      10 + p.x * minimapScale - 2,
+      (10 + p.x * minimapScale - 2),
       10 + p.y * minimapScale - 2,
       4,
       4
     );
+  
+    
+    //(a-b)  ((a-b)^2 )
+
+let startTime = performance.now();
+
+
+  }
+
+}
+let j = 0;
+
+function drawNextShrinkStep() {
+  if (j < 50) {
+    if (map[mapY]?.[mapX] === 0) {
+      ctx.fillRect(
+        canvas_center_x - j * 5,
+        canvas_center_y - j * 5,
+        500 - j * 5,
+        500 - j * 5
+      );
+    }
+    j++;
+    setTimeout(drawNextShrinkStep, 30); // 30 ms delay between steps
   }
 }
+
+//drawNextShrinkStep();
+  requestAnimationFrame(handleProjectilesAndDraw);
