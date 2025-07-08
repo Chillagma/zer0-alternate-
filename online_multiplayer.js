1768075f9b94d8f47a6599ab3b7a8e6d6e9cf89e
Chillagma <avatar129@hotmail.com>
2025-07-07 18:08:04 -0700

    done

--- online_multiplayer.js
+++ online_multiplayer.js
@@ -1,24 +1,19 @@
-const io = require("socket.io")(3000);
+const { Server } = require("socket.io");
+const io = new Server(3000);
 
 let players = {};
 
 io.on("connection", (socket) => {
   console.log("New player connected:", socket.id);
 
-  // Initialize player state
   players[socket.id] = { x: 100, y: 100 };
 
-  // Send current players to this new player
   socket.emit("currentPlayers", players);
-
-  // Broadcast new player to others
   socket.broadcast.emit("newPlayer", { id: socket.id, x: 100, y: 100 });
 
-  // Listen for player movement
   socket.on("playerMovement", (movementData) => {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
-    // Broadcast movement to others
     socket.broadcast.emit("playerMoved", { id: socket.id, x: movementData.x, y: movementData.y });
   });
 
